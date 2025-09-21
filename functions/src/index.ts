193e710df8d7a808a4a0e6bbf2bbf1d9e98f957e
koke-desu <gbpvp25645@yahoo.co.jp>
2025-09-21 23:14:23 +0900

    functions完成

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,32 +1,105 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import { messaging } from "firebase-admin";
+import { initializeApp } from "firebase-admin/app";
+import { getFirestore, Timestamp } from "firebase-admin/firestore";
+import { schedule } from "firebase-functions/v1/pubsub";
+import { onDocumentCreated } from "firebase-functions/v2/firestore";
 
-import {setGlobalOptions} from "firebase-functions";
-import {onRequest} from "firebase-functions/https";
-import * as logger from "firebase-functions/logger";
+initializeApp();
+const db = getFirestore();
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+type PlanDoc = {
+  uid: string;
+  dateKey: string; // 'YYYY-MM-DD'
+  dateTs?: Timestamp; // その日の 00:00
+  departures: string[]; // 'HH:mm' 1～2
+  items: string[]; // 1～5
+  allocation: "A" | "B" | "C";
+  createdAt: Timestamp;
+  updatedAt: Timestamp;
+};
 
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
+type UserDoc = {
+  displayName: string;
+  fcmToken: string;
+  createdAt: Timestamp;
+  updatedAt: Timestamp;
+};
 
-// export const helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const formatMinutesToHHmm = (minutes: number) => {
+  const h = Math.floor(minutes / 60);
+  const m = minutes % 60;
+  const hh = h < 10 ? `0${h}` : `${h}`;
+  const mm = m < 10 ? `0${m}` : `${m}`;
+  return `${hh}:${mm}`;
+};
+
+// 予定が立てられたら通知ドキュメントを追加
+export const addNotificationDoc = onDocumentCreated("plans/{planId}", async (event) => {
+  if (event.data === undefined) return;
+
+  const data = event.data.data() as PlanDoc;
+  const user = (await db.collection("users").doc(data.uid).get()).data() as UserDoc;
+  const notification = {
+    title: "持ち物をチェックしてください",
+    body: data.items.join("、"),
+    token: user.fcmToken,
+    time: "",
+  };
+  const notifications: (typeof notification)[] = [];
+  const minutes =
+    Number(data.departures[0].split(":")[0]) * 60 + Number(data.departures[0].split(":")[1]);
+
+  if (data.allocation === "A") {
+    notifications.push({ ...notification, time: formatMinutesToHHmm(minutes - 30) });
+    notifications.push({ ...notification, time: formatMinutesToHHmm(minutes - 20) });
+    notifications.push({ ...notification, time: formatMinutesToHHmm(minutes - 10) });
+  }
+  if (data.allocation === "B") {
+    notifications.push({ ...notification, time: formatMinutesToHHmm(minutes - 20) });
+    notifications.push({ ...notification, time: formatMinutesToHHmm(minutes - 10) });
+  }
+  if (data.allocation === "C") {
+    notifications.push({ ...notification, time: formatMinutesToHHmm(minutes - 10) });
+  }
+
+  for (const n of notifications) {
+    await db.collection("notifications").add(n);
+  }
+});
+
+// 1分毎に通知ドキュメントをチェックして、あればFCM送信＆ドキュメント削除
+export const checkNotification = schedule("every 1 minutes").onRun(async (context) => {
+  const now = new Date();
+  const hh = now.getHours();
+  const mm = now.getMinutes();
+  const nowHHmm = (hh < 10 ? `0${hh}` : `${hh}`) + ":" + (mm < 10 ? `0${mm}` : `${mm}`);
+
+  const snapshot = await db.collection("notifications").where("time", "==", nowHHmm).get();
+
+  if (snapshot.empty) {
+    console.log("No matching documents.");
+    return;
+  }
+
+  snapshot.forEach(async (doc) => {
+    const data = doc.data() as {
+      title: string;
+      body: string;
+      token: string;
+      time: string;
+    };
+    console.log(data);
+
+    // FCM送信
+    messaging().send({
+      token: data.token,
+      notification: {
+        title: data.title,
+        body: data.body,
+      },
+    });
+
+    // 通知ドキュメント削除
+    await db.collection("notifications").doc(doc.id).delete();
+  });
+});
