d16004032487e941ed8869a85698ba5afb1f1e09
koke-desu <gbpvp25645@yahoo.co.jp>
2025-09-21 18:54:26 +0900

    Qiitaと同じ状態に

---
+++ src/pages/test.tsx
@@ -0,0 +1,64 @@
+import { messaging } from "@/lib/firebase";
+import { getToken, isSupported, MessagePayload, onMessage } from "firebase/messaging";
+import React, { useEffect, useState } from "react";
+
+const useNotificationPermission = () => {
+  const [permission, setPermission] = useState<NotificationPermission>("default");
+  useEffect(() => {
+    const handler = () => setPermission(Notification.permission);
+    handler();
+    Notification.requestPermission().then(handler);
+    navigator.permissions.query({ name: "notifications" }).then((notificationPerm) => {
+      notificationPerm.onchange = handler;
+    });
+  }, []);
+  return permission;
+};
+
+const useFCMToken = () => {
+  const permission = useNotificationPermission();
+  const [fcmToken, setFcmToken] = useState<string | null>(null);
+  useEffect(() => {
+    const retrieveToken = async () => {
+      if (typeof window !== "undefined" && "serviceWorker" in navigator) {
+        if (permission === "granted") {
+          const isFCMSupported = await isSupported();
+          if (!isFCMSupported) return;
+          const fcmToken = await getToken(messaging, {
+            vapidKey: process.env.NEXT_PUBLIC_FIREBASE_VAPID_KEY,
+          });
+          setFcmToken(fcmToken);
+        }
+      }
+    };
+    retrieveToken();
+  }, [permission]);
+  return fcmToken;
+};
+
+const useFCM = () => {
+  const fcmToken = useFCMToken();
+  const [messages, setMessages] = useState<MessagePayload[]>([]);
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      const fcmMessaging = messaging;
+      const unsubscribe = onMessage(fcmMessaging, (payload) => {
+        setMessages((messages) => [...messages, payload]);
+      });
+      return () => unsubscribe();
+    }
+  }, [fcmToken]);
+  return { fcmToken, messages };
+};
+
+const Test: React.FC = ({}) => {
+  const { messages, fcmToken } = useFCM();
+  console.log(`messages:`, messages);
+  return (
+    <div>
+      <p>fcmToken: {fcmToken}</p>
+      <p>messages: {JSON.stringify(messages)}</p>
+    </div>
+  );
+};
+export default Test;
